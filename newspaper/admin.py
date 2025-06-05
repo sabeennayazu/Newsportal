2f45be6ca65df17f0158f0b573b32b32e84b861c
sabeen nayazu <sabeennayazu000@gmail.com>
2025-06-05 12:22:31 +0545

    first commit

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from newspaper.models import Advertisement, Category, Tag, Post
+from newspaper.models import Advertisement, Category, Tag, Post, UserProfile
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Post)
 admin.site.register(Advertisement)
+admin.site.register(UserProfile)
