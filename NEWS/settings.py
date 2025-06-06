36b4b705d1fbe398d779ece22f2c6b4dc926aa49
sabeen nayazu <sabeennayazu000@gmail.com>
2025-06-06 13:23:28 +0545

    second commit

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = ['static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
